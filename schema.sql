fd0e3b0e72bbb7880678831d0a83dfae3eb99f70
awakekibbles <gcolareta@gmail.com>
2022-07-02 14:06:23 -0400

    modified titles table

--- schema.sql
+++ schema.sql
@@ -50,9 +50,8 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no) 
+); -- emp_no CAN'T be a primary key. Primary Keys have a UNIQUE constrain
 
 SELECT * FROM departments;
 
